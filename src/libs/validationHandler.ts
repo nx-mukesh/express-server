42a28cf01604b5de38332471d08f05d726ae3c38
mukesh.maurya9 <mukesh.maurya@successive.tech>
2021-09-08 01:05:16 +0530

    feat:Generic Routes Validations

---
+++ src/libs/validationHandler.ts
@@ -0,0 +1,16 @@
+const { checkSchema, validationResult } = require('express-validator/check');
+
+const validationHandler = (validator) => {
+  return [
+    checkSchema(validator),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        next({ message: 'Bad Request', status: 422, error: errors.array() });
+      }
+      next();
+    },
+  ];
+};
+
+export default validationHandler;
